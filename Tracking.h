3e28ecdfe88c6007438c189f09dfe14d6a0bb1cc
SpyrosLavetzis <spyroslavetzis@gmail.com>
2020-11-13 23:10:37 +0200

    Create Tracking.h

---
+++ Tracking.h
@@ -0,0 +1,22 @@
+#pragma once //same as ifndef
+#include <iostream>
+#include <stdio.h>
+#include <opencv2\videoio.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\opencv.hpp>
+#include <opencv2\core.hpp>
+#include <opencv2\tracking.hpp>
+
+using namespace cv;
+
+class Tracking {
+private:
+	Mat frame; //create frame object
+	Ptr<Tracker> tracker;
+	Rect2d trackBox;
+	Point point;
+	VideoCapture cap;
+public:
+	Tracking();
+	void TrackingRun();
+};
