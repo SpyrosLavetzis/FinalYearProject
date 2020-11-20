ad278d39378febee8339bb7d9e18fa9f9f4779f0
SpyrosLavetzis <spyroslavetzis@gmail.com>
2020-11-20 19:48:53 +0200

    Update Tracking.cpp

--- Tracking.cpp
+++ Tracking.cpp
@@ -1,11 +1,11 @@
 #include "Tracking.h"
 using namespace std;
-
+//VideoCapture cap("C:\\Users\\spyro\\Desktop\\RealAnt1.MOV");
 Tracking::Tracking() {
-	//VideoCapture cap("C:\\Users\\spyro\\Desktop\\Ant.mp4"); // open video file
+	cap.open("C:\\Users\\spyro\\Desktop\\Ants\\FakeAnt1.MOV"); // open video file
 	//cap.open("C:\\Users\\spyro\\Desktop\\test.mp4"); 
 		//static const string windowName = "Test Video";
-	cap.open(0);
+	//cap.open(0);
 	//tracker = TrackerGOTURN::create(); //create algorithm specific tracker
 	tracker = TrackerMOSSE::create();
 	cap.read(frame);
@@ -15,7 +15,17 @@ Tracking::Tracking() {
 	resizeWindow("Test", 1000, 700);
 }
 
+void Tracking::SaveVideo() {
+	S = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),    // Acquire input size
+		(int)cap.get(CAP_PROP_FRAME_HEIGHT));
+	ex = static_cast<int>(cap.get(CAP_PROP_FOURCC));
+	video.open("C:\\Users\\spyro\\Desktop\\tracker3.MOV", ex, cap.get(CAP_PROP_FPS), S, true);
+	//this needs to be called for every frame.
+	video.write(frame);
+}
+
 void Tracking::TrackingRun() {
+
 	for (;;) {
 
 		cap.read(frame);
@@ -26,9 +36,10 @@ void Tracking::TrackingRun() {
 		if (tracker->update(frame, trackBox)) {//update tracking to next frame
 			rectangle(frame, trackBox, Scalar(255, 0, 0), 2, 8); //draw rectangle around object
 		}
-		Point point = trackBox.br(); //bottom right corner of ROI
-		//no matter the ration, br end of frame at 630-640
-		cout << point << endl;
+		/*Point point = trackBox.br(); //bottom right corner of ROI
+		//no matter the ration, br end of frame at 630-640 for pc cam
+		cout << point << endl;*/
+
 		imshow("Test", frame); //show frame with box
 		if (waitKey(1) == 27) { //update every 1 ms and break if esc key is pressed
 			break;
