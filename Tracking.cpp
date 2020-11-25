78050cc2e540d23cedb1426c11fdd875b1900d1a
SpyrosLavetzis <spyroslavetzis@gmail.com>
2020-11-25 13:52:51 +0200

    Update Tracking.cpp

--- Tracking.cpp
+++ Tracking.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 //VideoCapture cap("C:\\Users\\spyro\\Desktop\\RealAnt1.MOV");
 Tracking::Tracking() {
-	cap.open("C:\\Users\\spyro\\Desktop\\Ants\\FakeAnt1.MOV"); // open video file
+	cap.open("C:\\Users\\spyro\\Desktop\\Ants\\RealAnt1.MOV"); // open video file
 	//cap.open("C:\\Users\\spyro\\Desktop\\test.mp4"); 
 		//static const string windowName = "Test Video";
 	//cap.open(0);
 	//tracker = TrackerGOTURN::create(); //create algorithm specific tracker
 	tracker = TrackerMOSSE::create();
-	cap.read(frame);
+	//cap.read(frame);
 	namedWindow("Test", WINDOW_NORMAL);
 	resizeWindow("Test", 1000, 700);
-	trackBox = selectROI("Test", frame, false, false);
-	tracker->init(frame, trackBox);	//initialise tracker
+	//trackBox = selectROI("Test", frame, false, false);
+	//tracker->init(frame, trackBox);	//initialise tracker
 }
 
 void Tracking::SaveVideo() {
@@ -21,7 +21,7 @@ void Tracking::SaveVideo() {
 	ex = static_cast<int>(cap.get(CAP_PROP_FOURCC));
 	video.open("C:\\Users\\spyro\\Desktop\\tracker3.MOV", ex, cap.get(CAP_PROP_FPS), S, true);
 	//this needs to be called for every frame.
-	video.write(frame);
+	//video.write(frame);
 }
 
 void Tracking::TrackingRun() {
@@ -32,22 +32,30 @@ void Tracking::TrackingRun() {
 		if (frame.empty()) { //ensure we aren't stuck in the for loop foreve (after video is done)
 			break;
 		}
+		char c = 'i';
+		if ((waitKey(1) & 0xff) == int(c)) { //if i is pressed select ROI
+			tracker = TrackerMOSSE::create();
+			trackBox = selectROI("Test", frame, false, false);
+			tracker->init(frame, trackBox);	//initialise tracker
+		}
 		
+
 		if (tracker->update(frame, trackBox)) {//update tracking to next frame
 			rectangle(frame, trackBox, Scalar(255, 0, 0), 2, 8); //draw rectangle around object
 		}
-		else { //if algo fails redraw
+		/*else { //if algo fails redraw
 			cout << "here" << endl; 
 			tracker->clear();
 			tracker = TrackerMOSSE::create();
 			//trackBox = selectROI("Test", frame, false, false);
 			tracker->init(frame, trackBox);
-		}
+		}*/
 
-		/*Point point = trackBox.br(); //bottom right corner of ROI
+		/*Point point = trackBox.tl(); //bottom right corner of ROI
+		
 		//no matter the ration, br end of frame at 630-640 for pc cam
-		cout << point << endl;*/
-
+		cout << point << << endl;*/
+		//video.write(frame);
 		imshow("Test", frame); //show frame with box
 	
 		if (waitKey(1) == 27) { //update every 1 ms and break if esc key is pressed
