d33a04f9c1b3b4a527d2ba31a59bfd3b38e3ae1c
SpyrosLavetzis <spyroslavetzis@gmail.com>
2020-12-06 20:50:12 +0200

    Update Tracking.cpp

--- Tracking.cpp
+++ Tracking.cpp
@@ -1,5 +1,6 @@
 #include "Tracking.h"
 using namespace std;
+#include <string>
 //VideoCapture cap("C:\\Users\\spyro\\Desktop\\RealAnt1.MOV");
 Tracking::Tracking() {
 	cap.open("C:\\Users\\spyro\\Desktop\\Ants\\RealAnt1.MOV"); // open video file
@@ -8,12 +9,22 @@ Tracking::Tracking() {
 	//cap.open(0);
 	//tracker = TrackerGOTURN::create(); //create algorithm specific tracker
 	tracker = TrackerMOSSE::create();
-	//cap.read(frame);
+	cap.read(frame);
+	//tracker = TrackerMOSSE::create();
 	namedWindow("Test", WINDOW_NORMAL);
 	resizeWindow("Test", 1000, 700);
+	track_box = selectROI("Test", frame, false, false);
+	tracker->init(frame, track_box);
+	frame(track_box).copyTo(template_img);
+	//Rect box((track_box.x - 100), (track_box.y - 100), (track_box.width + 200), (track_box.height + 200));
+	//frame(box).copyTo(img);
+	//imshow("sure", img);
+	//frame.copyTo(img);
+	
+	
 	//trackBox = selectROI("Test", frame, false, false);
 	//tracker->init(frame, trackBox);	//initialise tracker
-	template_img = Mat();
+	//template_img = Mat();
 	frame_tracker = 0;
 }
 
@@ -34,7 +45,8 @@ void Tracking::TrackingRun() {
 		if (frame.empty()) { //ensure we aren't stuck in the for loop foreve (after video is done)
 			break;
 		}
-		char c = 'i';
+		frame_tracker++;
+		/*char c = 'i';
 		if ((waitKey(1) & 0xff) == int(c)) { //if i is pressed select ROI
 			tracker = TrackerMOSSE::create();
 			track_box = selectROI("Test", frame, false, false);
@@ -47,21 +59,54 @@ void Tracking::TrackingRun() {
 			
 			frame(track_box).copyTo(template_img);
 			//template_img.resize(trackBox.size());
-			imshow("Template", template_img);
+			//imshow("Template", template_img);
 			//imposter = template_img.clone();
-		}
-		
-
-		if (tracker->update(frame, track_box)) {//update tracking to next frame
-			rectangle(frame, track_box, Scalar(255, 0, 0), 2, 8); //draw rectangle around object
-			frame_tracker++;
-		}
+		}*/
 		if (frame_tracker == 10) {
 			frame_tracker = 0;
 			Rect box((track_box.x - 100), (track_box.y - 100), (track_box.width + 200), (track_box.height + 200));
 			frame(box).copyTo(img);
+			//frame.copyTo(img);
+			//img = Mat(frame,box);
+			Mat img_display;
+			img.copyTo(img_display);
+			Mat result;
+			int result_cols = img.cols - template_img.cols + 1;
+			int result_rows = img.rows - template_img.rows + 1;
+			result.create(result_rows, result_cols, CV_32FC1);
+			//string match_method = "TM_CCOEFF_NORMED";
+			matchTemplate(img, template_img, result, 5);
 
+			normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+			double minVal; double maxVal; Point minLoc; Point maxLoc;
+			Point matchLoc;
+			minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+			//if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+			//{
+				//matchLoc = minLoc;
+			//}
+			//else
+			//{
+			matchLoc = maxLoc;
+			//}
+			//rectangle(img_display, matchLoc, Point(matchLoc.x + template_img.cols, matchLoc.y + template_img.rows), Scalar::all(0), 2, 8, 0);
+			//rectangle(result, matchLoc, Point(matchLoc.x + template_img.cols, matchLoc.y + template_img.rows), Scalar::all(0), 2, 8, 0);
+			//imshow("Initial Window", img_display);
+			//imshow("Result", result);
+			Rect template_box(matchLoc.x, matchLoc.y, template_img.cols, template_img.rows);
+			frame(template_box).copyTo(template_img);
+			imshow("small image", img);
+			imshow("template", template_img);
+			/*track_box = template_box;
+			tracker = TrackerMOSSE::create();
+			tracker->init(frame, track_box);*/
 		}
+
+		if (tracker->update(frame, track_box)) {//update tracking to next frame
+			rectangle(frame, track_box, Scalar(255, 0, 0), 2, 8); //draw rectangle around object
+			
+		}
+		
 		/*else { //if algo fails redraw
 			cout << "here" << endl; 
 			tracker->clear();
@@ -78,7 +123,7 @@ void Tracking::TrackingRun() {
 		imshow("Test", frame); //show frame with box
 		//imshow("Template", template_img);
 	
-		if (waitKey(1) == 27) { //update every 1 ms and break if esc key is pressed
+		if (waitKey(50) == 27) { //update every 1 ms and break if esc key is pressed
 			break;
 		}
 	}
