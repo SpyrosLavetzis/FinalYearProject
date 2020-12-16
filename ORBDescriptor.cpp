6b88de5b8b98341bebb9e0234d4576ec1033ed9b
SpyrosLavetzis <spyroslavetzis@gmail.com>
2020-12-16 18:04:16 +0200

    Update ORBDescriptor.cpp

--- ORBDescriptor.cpp
+++ ORBDescriptor.cpp
@@ -3,10 +3,11 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2\tracking.hpp>
 #include <iostream>
+
 using namespace std;
 using namespace cv;
 
-void featureMatching(Mat img1, Mat img2) {
+void featureMatching(Mat img1, Mat img2, Mat mask) {
 	/*Mat img1;
 	Mat img2;*/
 	//this should change to use the frame and roi from MOSSE
@@ -16,23 +17,39 @@ void featureMatching(Mat img1, Mat img2) {
 	img1 = img2(box);*/
 	
 	Ptr<ORB> orb;
-	orb = ORB::create();
+	orb = ORB::create(500, 1.1f, 8, 31, 0, 4, ORB::HARRIS_SCORE, 31, 20);
 	vector<KeyPoint> img1_keypoints, img2_keypoints;
 	Mat img1_descriptor, img2_descriptor;
 
-	orb->detectAndCompute(img1, noArray(), img1_keypoints, img1_descriptor);
-	orb->detectAndCompute(img2, noArray(), img2_keypoints, img2_descriptor);
+	/*orb->detectAndCompute(img1, noArray(), img1_keypoints, img1_descriptor);
+	orb->detectAndCompute(img2, noArray(), img2_keypoints, img2_descriptor);*/
+
+	orb->detectAndCompute(img1, mask, img1_keypoints, img1_descriptor);
+	orb->detectAndCompute(img2, mask, img2_keypoints, img2_descriptor);
 
 	Ptr<BFMatcher> bf;
-	bf = BFMatcher::create(NORM_HAMMING, true);
+	bf = BFMatcher::create(NORM_HAMMING2, true);
 	vector<DMatch> matches;
 	bf->match(img1_descriptor, img2_descriptor, matches);
 
-	sort(matches.begin(), matches.end());
-    matches.resize(2);
+	/*sort(matches.begin(), matches.end());
+    matches.resize(3);*/
+	float min_dist = 1000, max_dist = 0; //for max function later
+	for (int i = 0; i < matches.size(); i++) {
+		float dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
+	cout << "Min:" << min_dist << "Max:" << max_dist << endl;
+	vector<DMatch> good_matches;
+	for (int i = 0; i < matches.size(); i++) {
+		if (matches[i].distance <= (1.2 * min_dist)) {
+			good_matches.push_back(matches[i]);
+		}
+	}
 
 	Mat img3;
-	drawMatches(img1, img1_keypoints, img2, img2_keypoints, matches, img3);
+	drawMatches(img1, img1_keypoints, img2, img2_keypoints, good_matches, img3);
 	//while (waitKey(1) < 1) {
 		imshow("image", img3);
 	//}
