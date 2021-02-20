cd79da2b118ad270a406a2367c58e17d298bda7c
SpyrosLavetzis <spyroslavetzis@gmail.com>
2021-02-21 01:19:10 +0200

    Update SparseOptFlow.cpp

--- SparseOptFlow.cpp
+++ SparseOptFlow.cpp
@@ -1,13 +1,13 @@
 #include "SparseOptFlow.h"
-
+void selectPoint(int event, int x, int y, int flags, void* userdata);
 SparseOptFlow::SparseOptFlow() {
-    //VideoCapture capture("C:\\Users\\spyro\\Desktop\\Ants\\ZoomedSlow.mp4");
-    
+    //capture.open("C:\\Users\\spyro\\Desktop\\Ants\\ZoomedSlow.mp4");
+
     capture.open(0);
     if (!capture.isOpened()) {
         //error in opening the video input
         cout << "Unable to open file!" << endl;
-       // break;
+        // break;
     }
 
     //create random colour using the rgb format
@@ -25,30 +25,61 @@ SparseOptFlow::SparseOptFlow() {
     //create window
     namedWindow("Test", WINDOW_NORMAL);
     resizeWindow("Test", 1000, 700);
-    for (int i = 0; i < 1; i++) {
+    /*for (int i = 0; i < 1; i++) {
         Rect box = selectROI("Test", old_frame, false, false);
         p0.push_back(box.br());
-    }
+    }*/
     // Create a mask image for drawing purposes
-    
     mask = Mat::zeros(old_frame.size(), old_frame.type());
-}
+    frame_counter = 0;
+    setMouseCallback("Test", selectPoint, this);
+    /*imshow("Test", old_frame);
+    waitKey(0);*/
     
+}
+
+
+    
+void selectPoint(int event, int x, int y, int flags, void *userdata ) {
+    Point point;
+    if (event == EVENT_LBUTTONDOWN) {
+        point = Point(x, y);
+        //vector<Point2f>* p0 = (vector<Point2f>*)userdata;
+        SparseOptFlow* obj = (SparseOptFlow*) userdata;
+        obj->p0.push_back(point);
+        obj->trackOptFlow();
+    }
+
+}
+
+void SparseOptFlow::playVideo() {
+    for (;;) {
+        //timer = (double)getTickCount();
+        capture.read(frame);
+       // freq = getTickFrequency() / ((double)getTickCount() - timer);
+        //putText(frame, to_string(freq), Point(100, 80), FONT_HERSHEY_PLAIN, 1.5, Scalar(0, 0, 255), 2);
+        imshow("Test", frame);
+        if (waitKey(1) == 27) { //update every 1 ms and break if esc key is pressed
+            break;
+        }
+    }
+}
+
 void SparseOptFlow::trackOptFlow() {
     while (true) {
-        
         timer = (double)getTickCount();
-        
         capture.read(frame);
         if (frame.empty())
             break;
+        frame_counter += 1;
         cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
         // calculate optical flow
 
         
-        criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
+        //criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
+        criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 30, 0.01);
 
-        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15, 15), 2, criteria);
+        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(21, 21), 4, criteria);
         //vector<Point2f> good_new;
         //cout << status.size() << endl;
         for (uint i = 0; i < p0.size(); i++)
@@ -69,7 +100,7 @@ void SparseOptFlow::trackOptFlow() {
         imshow("Test", img);
         
         keyboard = waitKey(1);
-        if (keyboard == 'q' || keyboard == 27)
+        if (keyboard == 27)
             break;
         // Now update the previous frame and previous points
         old_gray = frame_gray.clone();
@@ -77,6 +108,13 @@ void SparseOptFlow::trackOptFlow() {
         //reset vector as we only want to store current and previous position (not the ones before that)
         good_new.clear();
     }
+}
+
+void SparseOptFlow::updatePoints() {
+    if (frame_counter == 30) {
+
+    }
+
 }
 
 
